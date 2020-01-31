6081cac7c2d728e80551a1771a782934e9ade8f6
Ron Evans <ron@hybridgroup.com>
2020-01-31 18:20:42 +0100

    release: update to version 0.12.0
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- tinygo.rb
+++ tinygo.rb
@@ -1,14 +1,14 @@
 class Tinygo < Formula
     desc "TinyGo is a Go compiler for small places. Microcontrollers, WebAssembly, and command-line tools. Based on LLVM."
     homepage "https://tinygo.org/"
-    version "0.11.0"
+    version "0.12.0"
   
     if OS.mac?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-amd64.tar.gz"
-        sha256 "154a0f6e7c82aa4a1f4875973aeae62b87d44f6d9d09b49de33a623a4687abd9"
+        sha256 "890d074c1852a0d704040830cd7938d689fe8e2435f6bb27e0f0c172b221d029"
     elsif OS.linux?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.linux-amd64.tar.gz"
-        sha256 "01f3d5bb3962152a7fc18119f6e332cd5ef395181bd101cd35b61f575057dea9"
+        sha256 "2dc9a4b9f455b62282034f6d1b97fd1aa494acb8276b9e81b31e6290f30113c9"
     end
     
     bottle :unneeded
