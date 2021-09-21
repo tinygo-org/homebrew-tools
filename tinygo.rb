cc01e02694c40e23739e658f6d0bba1f0197d8a2
deadprogram <ron@hybridgroup.com>
2021-09-22 00:50:19 +0200

    release: update to version 0.20.0
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tinygo.rb
+++ tinygo.rb
@@ -1,14 +1,14 @@
 class Tinygo < Formula
     desc "TinyGo is a Go compiler for small places. Microcontrollers, WebAssembly, and command-line tools. Based on LLVM."
     homepage "https://tinygo.org/"
-    version "0.19.0"
+    version "0.20.0"
   
     if OS.mac?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-amd64.tar.gz"
-        sha256 "2c648b53c0040c1c78b00f30230761b6335165c02ee568608718be08ff844b5c"
+        sha256 "5071ddb4d293632c0fef09a44edfb074ff7a6f9d83ef0fb75b09c860c97aca91"
     elsif OS.linux?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.linux-amd64.tar.gz"
-        sha256 "61e6a8583e7150f4f383512b5a72469709917dacd66e29d05084f0ade60ead18"
+        sha256 "3b4130f0c5b3a22173c147e90629ea0a0bb52ec4bb50d309ff77bd19ccc80476"
     end
     
     bottle :unneeded
