a1ab663037251207447029e14219de5e73a2371a
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-07-01 12:46:44 +0200

    release: update to version 0.24.0

--- tinygo.rb
+++ tinygo.rb
@@ -1,16 +1,16 @@
 class Tinygo < Formula
     desc "TinyGo is a Go compiler for small places. Microcontrollers, WebAssembly, and command-line tools. Based on LLVM."
     homepage "https://tinygo.org/"
-    version "0.23.0"
+    version "0.24.0"
 
     depends_on "binaryen"
   
     if OS.mac?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-amd64.tar.gz"
-        sha256 "6d59543a0a3e917c9bdcb3b6fd9dbeb365d47b896d0afe8d710833aa61ea37d0"
+        sha256 "12b514cfb5bc0344396d598cecff05f0c71700edd2d078e816cf8191db6e809a"
     elsif OS.linux?
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.linux-amd64.tar.gz"
-        sha256 "decce0914ac5ee18047772fd07b95758657281e65a243af3963da52b9af3a4f8"
+        sha256 "0c95010c1b9452def4169a7794684f409a88ec3ce92b42fcfbab7f1a6357d2b0"
     end
 
     def install
