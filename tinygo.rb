ec9b36beb3263b5cd605ce749f0972d7edbb6992
deadprogram <ron@hybridgroup.com>
2024-02-26 18:28:42 +0100

    Update for release 0.31
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tinygo.rb
+++ tinygo.rb
@@ -1,16 +1,24 @@
 class Tinygo < Formula
     desc "TinyGo is a Go compiler for small places. Microcontrollers, WebAssembly, and command-line tools. Based on LLVM."
     homepage "https://tinygo.org/"
-    version "0.30.0"
+    version "0.31.0"
 
     depends_on "binaryen"
   
-    if OS.mac?
-        url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-amd64.tar.gz"
-        sha256 "6767934acc2d0a1b29b110de098dcf056b8cca1ce2737f01bc137c5d1f4f1ad7"
-    elsif OS.linux?
+    on_macos do
+        on_arm do
+            url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-arm64.tar.gz"
+            sha256 "7333de22885c0b492f2e7c106f29478a5f64632b14395880f3be6d6cc141617e"
+        end
+        on_intel do
+            url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.darwin-amd64.tar.gz"
+            sha256 "ed871e55a57b7c0e0304a215214fc507bdd68a4655d99053f344463360183bd5"
+        end
+      end
+    end
+    on_linux do
         url "https://github.com/tinygo-org/tinygo/releases/download/v#{version}/tinygo#{version}.linux-amd64.tar.gz"
-        sha256 "5d33610b6508b577761a7a134b0a38373232a807bf2abd97787583806a3b4920"
+        sha256 "d2ef8ef3c502147a757a51fbff05272edf082588366bb97b21c38c3d72eb6ca3"
     end
 
     def install
